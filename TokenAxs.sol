5e63f5a57cb5b79c061ab41a75109e83580ac4ce
Hantsuo <791691@qq.com>
2022-07-12 01:50:41 +0800

    lesson2.2

---
+++ TokenAxs.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract Token {
+    string public name = "axs";
+    string public symbol = "AXS";
+    uint256 public decimal = 18;
+    uint256 public _maxSupply = 21000000 *10 ** 18;
+    address public minter;  // 铸造地址
+    mapping(address => uint) public _balance; //地址余额映射
+    uint public _totalSupply;
+    constructor(address _minter) {
+        minter = _minter;
+    }
+    
+
+    modifier onlyOwner() {
+        require (msg.sender == minter,"not is own account" );
+        _;
+    }
+
+    function send(address get, uint amount) public{
+        require(_balance[msg.sender] >= amount,"not enougt balance");
+        _balance[msg.sender] -= amount;
+        _balance[get] += amount;
+        
+    }
+    function mint(address receiver, uint amount) public onlyOwner{
+        require(_totalSupply + amount <= _maxSupply,"over maxsupply");
+        _totalSupply += amount;
+        _balance[receiver] += amount;
+    }
+   
+}   
--- TokenSell.sol
+++ TokenSell.sol
@@ -2,34 +2,44 @@
 pragma solidity ^0.8.0;
 
 import "./TokenUsdt.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Tokensell{
         uint256 public price = 1 * 10 * 18;
-        uint256 userbalance = 0;
         uint starttime;
         uint endtime;
-        address public axsToken = 0x0aa7726bc0cB085A3C4fbE3B09b91DD7E27f7fea;
+        address public axsToken = 0xBcdA1F3B99609780a5dc57699269B0089807c817;
+        address public Usdt = 0x9ae5bfE06e70Dd5879320118Fbc194855f6C298D;
         mapping(address => uint) public _balance;
-
-    function launch(uint _starttime , uint _endtime)external view{
+    
+    function getuserbalance(address userbalance)public view returns(uint256){
+        uint256 bal = IERC20(0x9ae5bfE06e70Dd5879320118Fbc194855f6C298D).balanceOf(userbalance);
+        return bal;
+    }
+    function launch(uint _starttime , uint _endtime)public view{
         require(_starttime >= block.timestamp);
         require(_endtime >= _starttime);
         require(_endtime <= block.timestamp + 30 days);
         
     }
-    function sale(address get,uint256 buynmu)public{
+    function sale(uint256 buynmu)public{
         require(block.timestamp >= starttime);
         require(block.timestamp <= endtime);
         uint256 paynum = buynmu * price / 10 ** 18;
-        IERC20(axsToken).transferFrom(msg.sender , address(this),paynum);
-        _balance[get] += buynmu;
+        IERC20(Usdt).transferFrom(msg.sender, address(this),paynum);
+        _balance[msg.sender] += buynmu;
     }
-    function wuthdraw()public{
+    function withdraw()public{
+        require(block.timestamp >= starttime);
+        require(block.timestamp <= endtime);
+        uint bal = IERC20(Usdt).balanceOf(address(this));
+        IERC20(Usdt).transfer(0xdDE601eBe61EC5fD0729125D4Fd89F0f3b32B296,bal);
+    }
+    function Userwithdraw()public{
         require(block.timestamp >= starttime);
         require(block.timestamp <= endtime);
         uint bal = IERC20(axsToken).balanceOf(address(this));
-        IERC20(axsToken).transfer(0xdDE601eBe61EC5fD0729125D4Fd89F0f3b32B296,bal);
+        IERC20(axsToken).transfer(msg.sender,bal);
     }
     
 }
