48800f30d647bd603966d69585fd81a976f6136a
Hantsuo <791691@qq.com>
2022-07-25 00:44:27 +0800

    lesson3.0

--- TokenSell.sol
+++ TokenSell.sol
@@ -4,42 +4,90 @@ pragma solidity ^0.8.0;
 import "./TokenUsdt.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Tokensell{
-        uint256 public price = 1 * 10 * 18;
-        uint starttime;
-        uint endtime;
+contract presale{
+        uint256 public price;
+        uint256 public starttime;
+        uint256 public endtime;
+        uint256 public userWithdrawTime;
         address public axsToken = 0xBcdA1F3B99609780a5dc57699269B0089807c817;
         address public Usdt = 0x9ae5bfE06e70Dd5879320118Fbc194855f6C298D;
-        mapping(address => uint) public _balance;
+        mapping(address => uint256) public _balances;
+        mapping(address => uint256) public userBuyTotalUsdt;
+        mapping(address => address) public parents;
+        uint public maxAmount = 100 * 10 ** 18;
+        uint inviteNum;
+        uint inviteLv1 = 5 * 10 * 17;
+        uint inviteLv2 = 3 * 10 * 17;
+        uint inviteLv3 = 2 * 10 * 17;
+        uint inviteLv4 = 1 * 10 * 17;
+        uint inviteLv5 = 5 * 10 * 16;
+
+
+
     
-    function getuserbalance(address userbalance)public view returns(uint256){
-        uint256 bal = IERC20(0x9ae5bfE06e70Dd5879320118Fbc194855f6C298D).balanceOf(userbalance);
-        return bal;
+    
+
+    struct presaleInfo {
+        uint starttime;
+        uint endtime;
+        uint price;
+
     }
-    function launch(uint _starttime , uint _endtime)public view{
-        require(_starttime >= block.timestamp);
-        require(_endtime >= _starttime);
-        require(_endtime <= block.timestamp + 30 days);
-        
+
+    PresaleInfo[] presaleInfos;
+    
+    constructor (uint256 _userWithdrawTime)public{
+        userWithdrawTime = _userWithdrawTime;
     }
-    function sale(uint256 buynmu)public{
-        require(block.timestamp >= starttime);
-        require(block.timestamp <= endtime);
-        uint256 paynum = buynmu * price / 10 ** 18;
-        IERC20(Usdt).transferFrom(msg.sender, address(this),paynum);
-        _balance[msg.sender] += buynmu;
+
+    function setUserWithdrawTime() public onlyOwner {
+        userWithdrawTime = _userWithdrawTime;
     }
-    function withdraw()public{
-        require(block.timestamp >= starttime);
-        require(block.timestamp <= endtime);
-        uint bal = IERC20(Usdt).balanceOf(address(this));
-        IERC20(Usdt).transfer(0xdDE601eBe61EC5fD0729125D4Fd89F0f3b32B296,bal);
+
+    function createPreSale(PresaleInfo memory persaleInfo) public onlyOwner {
+        persaleInfos.push(presaleInfo);
+
     }
-    function Userwithdraw()public{
-        require(block.timestamp >= starttime);
-        require(block.timestamp <= endtime);
-        uint bal = IERC20(axsToken).balanceOf(address(this));
+    function addInvite (address parentAddress) public {
+        require(parentAddress != msg.sender);
+        parents[parentAddress] = parentAddress;
+        for (i = 0;i < parentAddress.lengh; i++){
+            if (parentAddress[i] == msg.sender){
+                inviteNum++;
+            }
+        }
+    }
+    function sale(uint256 pid) public {
+        PreSaleInfo memory preSaleInfo = preSaleInfos[pid];
+        require(block.timestamp >= preSaleInfo.starttime);
+        require(block.timestamp <= preSaleInfo.endtime);
+        require(userBuyTotalUsdt[msg.sender] <= preSaleInfos.lengh * maxAmount);
+        uint256 getTokenNum = maxAmount / preSaleInfo.price;
+        IERC20(Usdt).transferFrom(msg.sender, address(this), maxAmount);
+        _balances[msg.sender] += getTokenNum;
+        userBuyTotalUsdt[msg.sender] += maxAmount;
+        if (inviteNum == 9){
+            _balances[msg.sender] += getTokenNum * inviteLv1;
+        }else if(inviteNum == 8){
+            _balances[msg.sender] += getTokenNum * inviteLv2;
+        }else if(inviteNum == 7){
+            _balances[msg.sender] += getTokenNum * inviteLv3;
+        }else if(inviteNum == 5){
+            _balances[msg.sender] += getTokenNum * inviteLv4;
+        }else if(inviteNum <= 4 || inviteNum > 1)  {
+            _balances[msg.sender] += getTokenNum * inviteLv4;
+        }
+    }
+    function Userwithdraw() public {
+        require(block.timestamp > userWithdrawTime);
+        uint bal = _balances[msg.sender];
+        _balances[msg.sender] = 0;
         IERC20(axsToken).transfer(msg.sender,bal);
+    }
+    function teamWithdraw()public onlyOwner{
+        require(block.timestamp > userWithdrawTime);
+        uint bal = IERC20(Usdt).balanceOf(address(this));
+        IERC20(Usdt).transfer(msg.sender,bal);
     }
     
 }
--- TokenUsdt.sol
+++ TokenUsdt.sol
@@ -1,34 +1,16 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract Token {
-    string public name = "USDT";
-    string public symbol = "USDT";
-    uint256 public decimal = 18;
-    uint256 public _maxSupply = 21000000 *10 ** 18;
-    address public minter;  // 铸造地址
-    mapping(address => uint) public _balance; //地址余额映射
-    uint public _totalSupply;
-    constructor(address _minter) {
-        minter = _minter;
-    }
-    
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    modifier onlyOwner() {
-        require (msg.sender == minter,"not is own account" );
-        _;
+contract TokenUsdt is Ownable, ERC20("USDT","USDT"){
+    uint256 _maxSupply = 100000000 * 10 * 18;
+    function mint(address to,uint256 amount)public onlyOwner{
+        require (totalSupply() + amount <= _maxSupply,"Over maxSupply");
+        _mint(to,amount);
     }
-
-    function send(address get, uint amount) public{
-        require(_balance[msg.sender] >= amount,"not enougt balance");
-        _balance[msg.sender] -= amount;
-        _balance[get] += amount;
-        
+    function maxSupply() public view returns(uint256){
+        return _maxSupply;
     }
-    function mint(address receiver, uint amount) public onlyOwner{
-        require(_totalSupply + amount <= _maxSupply,"over maxsupply");
-        _totalSupply += amount;
-        _balance[receiver] += amount;
-    }
-   
-}   
+}
